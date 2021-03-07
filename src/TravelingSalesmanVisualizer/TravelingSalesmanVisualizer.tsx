70963e679e82ee2846422d63f7b06a62225d08c1
Markus Halme <maba.96@windowslive.com>
2021-03-07 19:27:37 +0200

    Add basic node component with css

---
+++ src/TravelingSalesmanVisualizer/TravelingSalesmanVisualizer.tsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import Node from './Node/Node'
+
+export default class TravelingSalesmanVisualizer extends Component {
+
+    render() {
+        return(
+            <div>foo<Node></Node></div>
+            
+        );
+    }
+}
