faae709c6796e7d0dae1d574896a2ecccf84a5bd
Markus Halme <maba.96@windowslive.com>
2021-03-09 02:27:28 +0200

    Add clickhandler for node class and selected nodes array functionality to grid class

--- src/TravelingSalesmanVisualizer/Node/Node.tsx
+++ src/TravelingSalesmanVisualizer/Node/Node.tsx
@@ -1,10 +1,46 @@
 import React, { Component } from 'react';
+import NodeGrid from '../NodeGrid/NodeGrid';
 import './Node.css'
 
-export default class Node extends Component {
+interface State {
+    bgColor:string
+    selected:boolean
+}
+
+interface Props {
+    xPos:number,
+    yPos:number,
+    grid:NodeGrid
+}
+
+export default class Node extends Component<Props, State> {
+    constructor(props:any) {
+        super(props);
+        this.state = {
+            bgColor: "",
+            selected: false
+        }
+    }
+
+
+    handleClick = () => {
+        /* when node clicked select/unselect it and add/delete node from 
+        grid nodes */
+        if(this.state.selected == true) {
+            this.setState({selected: false});
+            this.setState({bgColor: ""});
+            this.props.grid.deleteNode(this);
+            return;
+        }
+        this.setState({selected: true});
+        this.setState({bgColor: "blue"});
+        this.props.grid.addNode(this);
+        
+    }
+
     render() {
         return(
-            <div className="node"></div>
+            <div className="node" style={{backgroundColor: this.state.bgColor}} onClick={this.handleClick}></div>
         );
     }
 }
--- src/TravelingSalesmanVisualizer/NodeGrid/NodeGrid.tsx
+++ src/TravelingSalesmanVisualizer/NodeGrid/NodeGrid.tsx
@@ -7,6 +7,7 @@ const GRID_HEIGHT = 20;
 
 interface State {
     nodes: any[];
+    selectedNodes: Node[];
 }
 
 interface Props {
@@ -17,7 +18,7 @@ export default class NodeGrid extends Component<Props, State> {
 
     constructor(props: any) {
         super(props);
-        this.state = { nodes: [] };
+        this.state = { nodes: [], selectedNodes: []};
     }
 
     componentDidMount() {
@@ -35,15 +36,29 @@ export default class NodeGrid extends Component<Props, State> {
 
     }
 
+    deleteNode(node:Node) {
+        //delete node from the selected nodes
+        var helper = this.state.selectedNodes;
+        var index = helper.indexOf(node);
+        if(index !== -1) {
+            helper.splice(index, 1);
+            this.setState({selectedNodes: helper});
+        }
+    }
+
+    addNode(node:Node) {
+        //add node to selected nodes
+        this.state.selectedNodes.push(node);
+        console.log(this.state.selectedNodes);
+    }
 
     render() {
         const {nodes} = this.state;
-        console.log(nodes);
         return(
             <div className='nodegrid'>
-                {nodes.map((row:any, index:any) => {
+                {nodes.map((row:any, rowInd:number) => {
                     return <div>
-                        {row.map((node:any, nodeInd:any) => <Node></Node>)}
+                        {row.map((node:any, colInd:number) => <Node xPos={rowInd} yPos={colInd} grid={this}></Node>)}
                     </div>
                 }
 
